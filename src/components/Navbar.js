fc6a8f1aebca14b09d25cd7ac57d17b66faee4a3
KishanKumarSolanki <kkishankumar9983@gmail.com>
2025-02-09 03:51:31 +0530

    all heading are in center

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar(props) {
   return (
-    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+    <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`} >
       <div className="container">
         <a className="navbar-brand" href="/"><h2>{props.title}</h2></a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -71,9 +71,9 @@ export default function TextForm(props) {
         document.body.removeChild(element);
         props.showAlert("Text downloaded!", "success");
     };
-    
-    
-    const [utterance, setUtterance] = useState(null);
+
+
+    const [setUtterance] = useState(null);
     const synth = window.speechSynthesis;
     const handleSpeak = () => {
         if (synth.speaking) {
@@ -86,12 +86,12 @@ export default function TextForm(props) {
             return;
         }
         const newUtterance = new SpeechSynthesisUtterance(text);
-        setUtterance(newUtterance); 
+        setUtterance(newUtterance);
         synth.speak(newUtterance);
         props.showAlert("Speaking your text!", "success");
     };
 
-    
+
     const handleTogglePauseResume = () => {
         if (!synth.speaking) return;
 
@@ -105,23 +105,23 @@ export default function TextForm(props) {
             props.showAlert("Speech Paused!", "success");
         }
     };
-    
-    
-    
+
+
+
     const [isBold, setBold] = useState(false);
     const [isItalic, setItalic] = useState(false);
     const [text, setText] = useState('');
     const [isPaused, setIsPaused] = useState(false);
-    
+
 
     return (
         <>
             <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
-                <h1 className='mb-4 '>{props.heading}</h1>
+                <h1 className='mb-4 text-center'>{props.heading}</h1>
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? '#13466e' : 'white', color: props.mode === 'dark' ? 'white' : '#042743', fontWeight: isBold ? 'bold' : 'normal', fontStyle: isItalic ? 'italic' : 'normal' }} id="myBox" rows="8"></textarea>
                 </div>
-                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1 " onClick={handleUpClick}>Convert to Uppercase</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handletitleClick}>Convert to TitleCase</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleSentenceClick}>Convert to SentenceCase</button>
@@ -134,12 +134,12 @@ export default function TextForm(props) {
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-1" onClick={handleTogglePauseResume}>{isPaused ? "Resume Speech" : "Pause Speech"}</button>
                 <button disabled={text.length === 0} className="btn btn-danger mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
             </div>
-            <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+            <div className="container my-3 text-center" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
                 <h3>Your Text Summary</h3>
                 <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} Minutes read</p>
                 <h2>Preview</h2>
-                <p  style={{ fontWeight: isBold ? "bold" : "normal", fontStyle: isItalic ? 'italic' : 'normal'}}>{text.length > 0 ? text : "Nothing to preview!"}</p>
+                <p style={{ fontWeight: isBold ? "bold" : "normal", fontStyle: isItalic ? 'italic' : 'normal' }}>{text.length > 0 ? text : "Nothing to preview!"}</p>
             </div>
         </>
     )
